83bb9201c11d88dde1038ed8271886e9595e72bf
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-02-18 21:47:55 +0100

    Update ft_iterative_factorial.c

--- C-day/C05/ex00/ft_iterative_factorial.c
+++ C-day/C05/ex00/ft_iterative_factorial.c
@@ -12,15 +12,15 @@
 int	ft_iterative_factorial(int nb)
 {
 	int	i;
-	int	alo;
+	int	ah;
 
 	i = 2;
-	alo = 1;
+	ah = 1;
 	if (nb == 1 || nb == 0)
 		return (1);
 	if (nb < 0)
 		return (0);
 	while (i <= nb)
-		alo = alo * i++;
-	return (alo);
+		ah = ah * i++;
+	return (ah);
 }
