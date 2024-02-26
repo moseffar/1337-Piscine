a0a705a9bf9e25bba238ddf6e6badd02304ff864
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-02-26 22:54:53 +0100

    Update ft_putstr.c

--- C-day/C04/ex01/ft_putstr.c
+++ C-day/C04/ex01/ft_putstr.c
@@ -13,12 +13,12 @@
 
 void	ft_putstr(char *str)
 {
-	int	a;
+	int	i;
 
-	a = 0;
-	while (str[a] != '\0')
+	i = 0;
+	while (str[i] != '\0')
 	{
-		write(1, &str[a], 1);
-		a++;
+		write(1, &str[i], 1);
+		i++;
 	}
 }
