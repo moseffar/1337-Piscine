9cb44f55642cef639a96ff40930ea98d9504c376
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-03-03 19:00:45 +0100

    Update ft_find_next_prime.c

--- C-day/C05/ex07/ft_find_next_prime.c
+++ C-day/C05/ex07/ft_find_next_prime.c
@@ -12,10 +12,10 @@
 int	ft_is_prime(int nb)
 {
 	int	i;
-	int	ah;
+	int	str;
 
 	i = 2;
-	ah = 1;
+	str = 1;
 	if (nb <= 1)
 		return (0);
 	if (nb > 2)
@@ -27,7 +27,7 @@ int	ft_is_prime(int nb)
 			i++;
 		}
 	}
-	return (ah);
+	return (str);
 }
 
 int	ft_find_next_prime(int nb)
