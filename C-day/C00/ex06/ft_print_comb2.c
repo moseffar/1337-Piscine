5ab4629f9941d3317b1533a4fd938127a345c562
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-03-07 08:20:51 +0100

    Update ft_print_comb2.c

--- C-day/C00/ex06/ft_print_comb2.c
+++ C-day/C00/ex06/ft_print_comb2.c
@@ -11,31 +11,31 @@
 /* ************************************************************************** */
 #include <unistd.h>
 
-void	putchar(char a)
+void	putchar(char i)
 {
-	write(1, &a, 1);
+	write(1, &i, 1);
 }
 
 void	ft_print_comb2(void)
 {
-	int	a;
+	int	i;
 	int	b;
 
-	a = 0;
-	while (a <= 98)
+	i = 0;
+	while (i <= 98)
 	{
-		b = a + 1;
+		b = i + 1;
 		while (b <= 99)
 		{
-			putchar((a / 10) + 48);
-			putchar((a % 10) + 48);
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
 			write(1, " ", 1);
 			putchar((b / 10) + 48);
 			putchar((b % 10) + 48);
 			b++;
-			if (a != 98)
+			if (i != 98)
 				write(1, ", ", 2);
 		}
-		a++;
+		i++;
 	}
 }
