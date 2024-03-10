9e9eb6002c1502beba98e24a958aae0373cf9d4b
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-03-10 16:46:09 +0000

    Update ft_strcmp.c

--- C-day/C03/ex00/ft_strcmp.c
+++ C-day/C03/ex00/ft_strcmp.c
@@ -11,12 +11,12 @@
 /* ************************************************************************** */
 int	ft_strcmp(char *s1, char *s2)
 {
-	unsigned int	i;
+	unsigned int	a;
 
-	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	a = 0;
+	while (s1[a] == s2[a] && s1[a] != '\0' && s2[a] != '\0')
 	{
-		i++;
+		a++;
 	}
-	return (s1[i] - s2[i]);
+	return (s1[a] - s2[a]);
 }
