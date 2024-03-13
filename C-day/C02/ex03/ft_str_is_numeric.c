37d7dd884fba5bbf1a2665bd60da9de585973a85
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-03-13 16:01:31 +0000

    Update ft_str_is_numeric.c

--- C-day/C02/ex03/ft_str_is_numeric.c
+++ C-day/C02/ex03/ft_str_is_numeric.c
@@ -11,14 +11,14 @@
 /* ************************************************************************** */
 int	ft_str_is_numeric(char *str)
 {
-	int	a;
+	int	i;
 
-	a = 0;
-	while (str[a] != '\0')
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (!(str[a] >= '0' && str[a] <= '9'))
+		if (!(str[i] >= '0' && str[i] <= '9'))
 			return (0);
-		a++;
+		i++;
 	}
 	return (1);
 }
