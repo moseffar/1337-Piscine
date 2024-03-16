2684020023dd2e54ce91849f77be7dc5d9dd9bc7
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-03-16 08:24:27 +0000

    Update ft_strcapitalize.c

--- C-day/C02/ex09/ft_strcapitalize.c
+++ C-day/C02/ex09/ft_strcapitalize.c
@@ -25,17 +25,17 @@ char	*ft_strlowcase(char *str)
 
 char	*ft_strcapitalize(char *str)
 {
-	int	i;
+	int	a;
 
 	ft_strlowcase(str);
-	i = 0;
-	while (str[i] != '\0')
+	a = 0;
+	while (str[a] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			if (str[i - 1] < '0' || (str[i - 1] > '9' && str[i - 1] < 'A')
-				|| (str[i - 1] > 'Z' && str[i - 1] < 'a') || str[i - 1] > 'z')
-				str[i] -= 32;
-		i++;
+		if (str[a] >= 'a' && str[a] <= 'z')
+			if (str[a - 1] < '0' || (str[a - 1] > '9' && str[a - 1] < 'A')
+				|| (str[a - 1] > 'Z' && str[a - 1] < 'a') || str[a - 1] > 'z')
+				str[a] -= 32;
+		a++;
 	}
 	return (str);
 }
