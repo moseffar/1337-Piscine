f4bbb521b0bdf975df02f3590aae1a838f9eb9db
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-03-17 20:40:32 +0000

    Update first.c

--- EXAMS/first.c
+++ EXAMS/first.c
@@ -3,13 +3,13 @@ int	main(int	ac, char	**av)
 {
 	if(ac == 2)
 	{
-		int	i=0;
-		while(av[1][i] == ' ' || av[1][i] == '	')
-			i++;
-		while(av[1][i] != ' ' && av[1][i] != '\t' && av[1][i] != '\0')
+		int	o=0;
+		while(av[1][o] == ' ' || av[1][o] == '	')
+			o++;
+		while(av[1][o] != ' ' && av[1][o] != '\t' && av[1][o] != '\0')
 		{
-			write(1, &av[1][i], 1);
-			i++;
+			write(1, &av[1][o], 1);
+			o++;
 		}
 	}
 	write(1, "\n", 1);
