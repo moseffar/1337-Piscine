7d568d2692bc45618b76e4780f8ab6c54bfb02d2
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-03-21 10:52:56 +0000

    Update ft_is_prime.c

--- C-day/C05/ex06/ft_is_prime.c
+++ C-day/C05/ex06/ft_is_prime.c
@@ -11,20 +11,20 @@
 /* ************************************************************************** */
 int	ft_is_prime(int nb)
 {
-	int	i;
+	int	a;
 	int	str;
 
-	i = 2;
+	a = 2;
 	str = 1;
 	if (nb <= 1)
 		return (0);
 	if (nb > 2)
 	{
-		while (i <= (nb / 2))
+		while (a <= (nb / 2))
 		{
-			if (nb % i == 0)
+			if (nb % a == 0)
 				return (0);
-			i++;
+			a++;
 		}
 	}
 	return (str);
