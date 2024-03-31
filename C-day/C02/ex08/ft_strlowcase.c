2afccf4ab198f09bf9334dbdd8d69b40777ab5b9
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-03-31 20:04:15 +0000

    Update ft_strlowcase.c

--- C-day/C02/ex08/ft_strlowcase.c
+++ C-day/C02/ex08/ft_strlowcase.c
@@ -11,14 +11,14 @@
 /* ************************************************************************** */
 char	*ft_strlowcase(char *str)
 {
-	int	a;
+	int	i;
 
-	a = 0;
-	while (str[a] != '\0')
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (str[a] <= 90 && str[a] >= 65)
-			str[a] += 32;
-		a++;
+		if (str[i] <= 90 && str[i] >= 65)
+			str[i] += 32;
+		i++;
 	}
 	return (str);
 }
