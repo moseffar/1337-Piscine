82eb7225ea43b627eaa918ab01e0e517ca8a49f5
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-04-12 19:56:06 +0000

    Update ft_fibonacci.c

--- C-day/C05/ex04/ft_fibonacci.c
+++ C-day/C05/ex04/ft_fibonacci.c
@@ -9,14 +9,14 @@
 /*   Updated: 2023/10/01 01:23:29 by moseffar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	ft_fibonacci(int indx)
+int	ft_fibonacci(int index)
 {
-	if (indx < 0)
+	if (index < 0)
 		return (-1);
-	if (indx == 0)
+	if (index == 0)
 		return (0);
-	if (indx == 1)
+	if (index == 1)
 		return (1);
 	else
-		return (ft_fibonacci(indx - 1) + ft_fibonacci(indx - 2));
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
