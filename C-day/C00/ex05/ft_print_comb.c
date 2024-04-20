745647abb89a8fcc60a8f5ca8dda2a8338b8849c
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-04-20 13:21:57 +0100

    Update ft_print_comb.c

--- C-day/C00/ex05/ft_print_comb.c
+++ C-day/C00/ex05/ft_print_comb.c
@@ -14,26 +14,26 @@
 void	ft_print_comb(void)
 {
 	char	i;
-	char	b;
-	char	c;
+	char	j;
+	char	k;
 
 	i = '0';
 	while (i <= '7')
 	{
-		b = i +1;
-		while (b <= '8')
+		j = i +1;
+		while (j <= '8')
 		{
-			c = b + 1;
-			while (c <= '9')
+			k = j + 1;
+			while (k <= '9')
 			{
 				write(1, &i, 1);
-				write(1, &b, 1);
-				write(1, &c, 1);
+				write(1, &j, 1);
+				write(1, &k, 1);
 				if (i != '7')
 					write (1, ", ", 2);
-				c++;
+				k++;
 			}
-			b++;
+			j++;
 		}
 		i++;
 	}
